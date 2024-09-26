3d68119b5db2b2006bfb44d5510975afdf22efa9
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-09-26 11:30:58 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "SIPSorcerySDL2Test";
+        packages = with pkgs; [
+          dotnet-sdk_8
+          ffmpeg
+          SDL2
+          SDL2.dev
+          pjsip
+          sngrep
+        ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.SDL2 ];
+      };
+    };
+}
