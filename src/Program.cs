d8f319eddb2ad12741db2dbce9147dd4bc00466f
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-09-30 14:45:38 +0200

    incomming calls handling

--- src/Program.cs
+++ src/Program.cs
@@ -7,14 +7,20 @@ using SIPSorceryMedia.SDL2;
 using SIPSorceryMedia.Abstractions;
 
 class Program {
-    static async Task Main(string[] args) {
+    static ManualResetEvent exitMre = new ManualResetEvent(false);
+
+    static void Main(string[] args) {
         var sipTransport = new SIPTransport();
 
         sipTransport.AddSIPChannel(new SIPUDPChannel(new IPEndPoint(IPAddress.Any, int.Parse(args[0]))));
 
-        var userAgent = new SIPUserAgent(sipTransport, null, true);
-        
+        var userAgent = new SIPUserAgent(sipTransport, null);
+
+        // string mic = SDL2Helper.GetAudioRecordingDevice("Plan");
+        // Console.WriteLine(mic);
+
         var audioSource = new FFmpegAudioSource(new AudioEncoder());
+        // var audioSource = new FFmpegMicrophoneSource($"audio={mic}", new AudioEncoder());
         audioSource.RestrictFormats(x => x.Codec == AudioCodecsEnum.PCMU);
 
         // string ffmpegBinPath = Environment.GetEnvironmentVariable("FFMPEG_BIN");
@@ -25,6 +31,7 @@ class Program {
         SDL2Helper.InitSDL();
 
         string deviceName = SDL2Helper.GetAudioPlaybackDevice("Plan");
+        Console.WriteLine(deviceName);
 
         var audioSink = new SDL2AudioEndPoint(deviceName, new AudioEncoder());
         // audioSink.SetAudioSinkFormat(new AudioFormat(SDPWellKnownMediaFormatsEnum.PCMU));
@@ -38,15 +45,51 @@ class Program {
         var voipMediaSession = new VoIPMediaSession(mediaEndPoints);
         voipMediaSession.AcceptRtpFromAny = true;
 
+
+        Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e) {
+            e.Cancel = true;
+
+            if (userAgent.IsCallActive) {
+                Console.WriteLine("Hanging up.");
+                userAgent.Hangup();
+            } else {
+                Console.WriteLine("Cancelling call.");
+                userAgent.Cancel();
+            }
+            exitMre.Set();
+        };
+
+        userAgent.OnIncomingCall += async (ua, req) => {
+            Console.WriteLine($"Incoming call from {req.RemoteSIPEndPoint}.");
+
+            var uas = ua.AcceptCall(req);
+
+            await ua.Answer(uas, voipMediaSession);
+
+            await audioSource.PauseAudio();
+            await voipMediaSession.AudioExtrasSource.StartAudio();
+            voipMediaSession.AudioExtrasSource.SetSource(AudioSourcesEnum.Music);
+        };
+
+        // MakeCall(userAgent, args[1], voipMediaSession);
+
+        exitMre.WaitOne();
+
+        sipTransport.Shutdown();
+
+    }
+
+    static async Task<bool> MakeCall(SIPUserAgent ua, string destination, VoIPMediaSession voipMediaSession) {
         Console.WriteLine("making call.");
-        var callResult = await userAgent.Call(args[1], null, null, voipMediaSession);
+        var callResult = await ua.Call(destination, null, null, voipMediaSession);
         Console.WriteLine($"Call result {((callResult) ? "success" : "failure")}.");
 
-        // await audioSource.PauseAudio();
-        // await voipMediaSession.AudioExtrasSource.StartAudio();
-        // voipMediaSession.AudioExtrasSource.SetSource(AudioSourcesEnum.Music);
-        
+        var audioSource = voipMediaSession.Media.AudioSource;
 
-        Console.ReadLine();
+        await audioSource.PauseAudio();
+        await voipMediaSession.AudioExtrasSource.StartAudio();
+        voipMediaSession.AudioExtrasSource.SetSource(AudioSourcesEnum.Music);
+
+        return callResult;
     }
 }
